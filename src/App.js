5e03032ce19b5344e9f60eaba17bf42b3265c23e
Nikica Novakovic <novakovicnikica@gmail.com>
2020-10-15 20:14:08 +0200

    Refactor App and data source

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import DataTable from './DataTable';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='container mt-3'>
-        <DataTable rows={this.props.rows} locale='da' rowsPerPage={5} />
-      </div>
-    );
-  }
-}
+import './App.css';
+import data from './data.json';
+
+const App = () => {
+  const [userData] = useState(data['user-data']);
+  const [locale] = useState('da');
+  const [rowsPerPage] = useState(5);
+
+  return (
+    <div className='container mt-3'>
+      <DataTable rows={userData} locale={locale} rowsPerPage={rowsPerPage} />
+    </div>
+  );
+};
 
 export default App;
